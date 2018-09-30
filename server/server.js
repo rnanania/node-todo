bd37d0fc7983478f6d41b4d890b6b02be7a2e95f
Rohit Nanania <nanania.rohit@gmail.com>
2018-09-30 17:40:43 -0400

    User model addition with jwt post

--- server/server.js
+++ server/server.js
@@ -94,6 +94,21 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+
+app.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+    let user = new User(body);
+
+    user.save().then((user) => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send({user});
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+
 app.listen(PORT, () => {
     console.log('Server started on port 3000');
 });
